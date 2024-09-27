63a735141b1f818e845245cd771a6db1739b6e24
aravind452 <aravindaru452@gmail.com>
2024-09-27 12:13:48 +0530

    added static code

---
+++ INTERMEDIATE JAVA - TELUKSO/StaticMethods.java
@@ -0,0 +1,36 @@
+class Mobile1 {
+  String brand;
+  int price;
+  static String name;
+
+  void show() {
+    System.out.println("Brand: " + brand + " Price: " + price + " Phone: " + name);
+  }
+
+  static void show1(Mobile1 m1) {
+    System.out.println("In static method");
+    System.out.println("Brand: " + m1.brand + " Price: " + m1.price + " Phone: " + name);
+
+  }
+}
+
+public class StaticMethods {
+
+  public static void main(String[] args) {
+    Mobile1 m1 = new Mobile1();
+    m1.brand = "Redmi";
+    m1.price = 45558;
+    Mobile1.name = "SmartPhone";
+
+    Mobile1 m2 = new Mobile1();
+    m2.brand = "Realme";
+    m2.price = 78000;
+    Mobile1.name = "SmartPhone";
+
+    m1.show();
+    m2.show();
+
+    Mobile1.show1(m1);
+  }
+
+}
---
+++ INTERMEDIATE JAVA - TELUKSO/StaticUseCases.java
@@ -0,0 +1,53 @@
+public class StaticUseCases {
+
+  public static void main(String[] args) throws ClassNotFoundException {
+    Mobile m1 = new Mobile();
+    m1.brand = "Redmi";
+    m1.price = 45558;
+    m1.color = "Black";
+    Mobile.name = "SmartPhone";
+
+    Mobile m2 = new Mobile();
+    m2.brand = "Realme";
+    m2.price = 78000;
+    m2.color = "Blue";
+    Mobile.name = "SmartPhone";
+
+    m1.show();
+    m2.show();
+
+    Class.forName("Mobile"); // used to load a class dynamically at runtime.
+
+  }
+
+}
+
+class Mobile {
+  String brand;
+  int price = 23;
+  String color;
+  static String name;
+
+  static { // Called only once
+    name = "Phone";
+    // Static variables should generally be initialized in a static context, like a
+    // static block
+    System.out.println("static block");
+
+  }
+
+  public Mobile() {
+    System.out.println("in constructor");
+    brand = "";
+    price = 2000;
+    color = "red";
+    // name = "Phone"; // Since static variables belong to the class and are shared
+    // by all instances,
+    // it is unusual to initialize a static variable within an instance-level
+    // constructor.
+  }
+
+  void show() {
+    System.out.println("Brand: " + brand + " Price: " + price + " color: " + color + " Phone: " + name);
+  }
+}
