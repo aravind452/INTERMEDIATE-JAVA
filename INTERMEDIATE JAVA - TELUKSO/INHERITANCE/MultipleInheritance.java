936f7b9402b7606ab8c67d5faf25c73e453d7f2e
aravind452 <aravindaru452@gmail.com>
2024-09-29 18:11:15 +0530

    added mutiple inheritance

---
+++ INTERMEDIATE JAVA - TELUKSO/INHERITANCE/MultipleInheritance.java
@@ -0,0 +1,22 @@
+public class MultipleInheritance {
+  public static void main(String[] args) {
+  }
+}
+
+class AAA {
+
+}
+
+class BBB {
+  public void display() {
+    System.out.println("BBB");
+  }
+
+}
+
+class CCC {
+  public void display() {
+    System.out.println("CCC");
+  }
+
+}
