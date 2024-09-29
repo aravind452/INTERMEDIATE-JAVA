9ad52e72f06d736b86e2ed4e5c5ac35972b77168
aravind452 <aravindaru452@gmail.com>
2024-09-29 15:33:36 +0530

    casting added

---
+++ INTERMEDIATE JAVA - TELUKSO/Casting.java
@@ -0,0 +1,25 @@
+public class Casting {
+  public static void main(String[] args) {
+
+    Obj1 a = new Obj2();
+    // Obj1 a = (Obj1) new Obj2(); upcasting // implicit
+    a.show1();
+
+    Obj2 b = (Obj2) a;
+    b.show2();
+
+  }
+
+}
+
+class Obj1 {
+  public void show1() {
+    System.out.println("Obj1");
+  }
+}
+
+class Obj2 extends Obj1 {
+  public void show2() {
+    System.out.println("Obj2");
+  }
+}
