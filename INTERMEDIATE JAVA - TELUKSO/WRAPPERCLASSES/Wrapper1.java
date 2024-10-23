8fc8965d1b0fd14dfff53552c0882997b1ba43c8
aravind452 <aravindaru452@gmail.com>
2024-10-23 15:27:03 +0530

    added wrapper classes

---
+++ INTERMEDIATE JAVA - TELUKSO/WRAPPERCLASSES/Wrapper1.java
@@ -0,0 +1,20 @@
+package WRAPPERCLASSES;
+
+public class Wrapper1 {
+
+  public static void main(String[] args) {
+
+    // int num = 10;
+    // Integer autoBoxednum = num; // Autoboxing
+
+    // int unBoxedNum = autoBoxednum; // unboxing
+
+    // String str = "100";
+    // int intNum = Integer.parseInt(str);
+    // Integer integer = Integer.valueOf(intNum);
+
+    // String str1 = String.valueOf(integer);
+
+  }
+
+}
