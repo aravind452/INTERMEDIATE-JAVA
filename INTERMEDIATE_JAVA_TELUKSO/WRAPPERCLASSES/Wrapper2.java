45439a83b90346d2cd27c161b2fdeb0e7edf0c00
aravind452 <aravindaru452@gmail.com>
2024-10-30 14:47:12 +0530

    added wrapper classes

---
+++ INTERMEDIATE_JAVA_TELUKSO/WRAPPERCLASSES/Wrapper2.java
@@ -0,0 +1,16 @@
+package WRAPPERCLASSES;
+
+public class Wrapper2 {
+
+  public static void main(String[] args) {
+    Integer a = 5;
+    Integer b = a;
+
+    a = 10;
+
+    System.out.println(a);
+    System.out.println(b);
+
+  }
+
+}
