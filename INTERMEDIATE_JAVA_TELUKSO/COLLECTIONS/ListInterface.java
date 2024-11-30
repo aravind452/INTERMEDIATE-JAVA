6cb2bedde6dcbf9c095ca9a232e8d8d838a395c8
aravind452 <aravindaru452@gmail.com>
2024-11-30 22:45:14 +0530

    added collections

--- INTERMEDIATE_JAVA_TELUKSO/COLLECTIONS/ListInterface.java
+++ INTERMEDIATE_JAVA_TELUKSO/COLLECTIONS/ListInterface.java
@@ -2,6 +2,18 @@ package COLLECTIONS;
 
 import java.util.*;
 
+/* List Interface 
+When you create an ArrayList in Java, you can set an initial capacity or let it default to a predefined value.
+
+Default Initial Capacity:
+If you create an ArrayList without specifying a capacity, the default initial capacity is 10.
+
+When the capacity is exceeded, the ArrayList increases its size by approximately 50% of its current capacity.
+
+For example, if the current capacity is 10, it will increase to 15 (10 + 5). If the capacity is 15, it increases to around 22 (15 + 7), and so on.
+
+*/
+
 public class ListInterface {
 
   public static void main(String[] args) {
